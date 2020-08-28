46d989e76cfee4a82562e402a3e7444ac2015bae
Aadithya Vasudevan <68108339+Aadithya-19@users.noreply.github.com>
2020-08-28 06:27:23 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,66 @@
+var dog, doghappy, dogImg2;
+var foodS;
+
+function preload()
+{
+  dogImg2 = loadImage("images/dogImg.png");
+  doghappy = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+
+  createCanvas(500, 500);
+  database = firebase.database();
+  dog = createSprite(250,250,10,10);
+  dog.addImage("dog_original", dogImg2);
+  dog.addImage("happy",doghappy);
+  dog.scale = 0.2;
+
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock, showError);
+}
+
+
+function draw() {  
+
+  background(46, 200, 87);
+  textSize(20);
+  text("Press the Up arrow to feed the dog", 100, 100);
+  text("Food left : " + foodS, 100, 150);
+  color(0, 0, 0);
+   
+  drawSprites();
+
+}
+
+function readStock(data){
+  foodS = data.val();
+  console.log(foodS);
+}
+
+ function writeStock(x){
+
+  if (x<=0){
+    x=0;
+  }
+  else{
+    x = x - 1;
+    database.ref('/').update({
+    Food : x
+    })
+  }
+ }
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    console.log(foodS);
+   writeStock(foodS);
+   
+   dog.changeImage("happy", doghappy);
+   }
+ }
+ 
+ function showError(){
+  console.log("hi");
+}
